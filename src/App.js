99ade0c659b50e5cde164bc53dd43fe0c39a6e68
MrAnyName <vhamlet0@gmail.com>
2022-11-18 22:52:53 +0400

    second comit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from "./Components/Main/main";
-
-
+//////////////////////////////////////////////////////
+// for starting in first terminal run npm run server
+// in second terminal npm start
+//////////////////////////////////////////////////////
 function App() {
   return (
     <div className="App">
